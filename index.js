cbea2d54b73427abb75dc85893c284b2fdb66929
Michael Fisher <mafish88@icloud.com>
2023-10-05 16:55:59 -0400

    created objects

---
+++ index.js
@@ -0,0 +1,21 @@
+const person ={
+    firstName: "Todd",
+    age: 28
+}
+//print the person's name
+console.log(person.firstName)
+
+//add one to the person's age
+person.age++
+
+console.log(person.age)
+
+person.lastName = "albert" // add a new property to the object
+/*
+const person = {
+    firstName: "todd"
+    age: 29
+    lastName: "albert"
+}
+*/
+console.log(person)
